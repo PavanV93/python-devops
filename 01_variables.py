70e940d7be1c23e084d64df1513926720b6d8ffd
pavan <vpavan7477@gmail.com>
2024-08-07 11:33:35 +0530

    yaml file

--- 01_variables.py
+++ 01_variables.py
@@ -2,3 +2,20 @@
 a = 36
 # print the value that is stored inside the variable
 print(a)
+
+# Float value
+b = 32.600
+print(b)
+
+# Boolean
+c = True # true
+
+# strings
+d = 'This is a strong'
+e = "This is a string"
+f = """
+This is a multi line string
+"""
+# Today weather is cool
+d = 'Today' weather is cool
+print (d)

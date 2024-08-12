6bfe3aa30eb5e8b1a292a6367618cf711129426f
pavan <vpavan7477@gmail.com>
2024-08-12 16:02:13 +0530

    commit backend files

---
+++ 09_usecase.py
@@ -0,0 +1,13 @@
+# login system
+username = "ec2-user"
+password = "DevOps321"
+
+in_username = input("please enter your username: ")
+in_password = input("please enter your_password: ")
+
+if (in_username == username) and (in_password == password):
+    print("Login is successful")
+else: 
+    print("Please check your credentials")
+
+    

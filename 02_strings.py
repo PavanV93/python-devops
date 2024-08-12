429c523b77c948a92a1c2cdb251d4043babb479a
pavan <vpavan7477@gmail.com>
2024-08-12 15:45:41 +0530

    commit backend files

--- 02_strings.py
+++ 02_strings.py
@@ -2,4 +2,10 @@ sample_str = "Thsis is sample string"
 print(sample_str)
 
 # How to acess individual character from a string
-print(sample_str(8))
+print(sample_str[8])
+
+#slicing
+sub_str = sample_str[2:7]
+print(sub_str)
+
+

553496aeafa634688f0a1f63a0f6f08e8a81e7fe
pavan <vpavan7477@gmail.com>
2024-08-12 16:17:44 +0530

    commit backend files

--- 10_loops.py
+++ 10_loops.py
@@ -1,5 +1,9 @@
 sample_list = ["ansible", "Terraform", "Jenkins", "Docker"]
 
-for ele in sample_list:
-    print(ele, len(ele))
-    
+# for ele in sample_list:
+#     print(ele, len(ele))
+
+# print the element and its corresponding index
+
+for ele in enumerate(sample_list):
+    print(ele)

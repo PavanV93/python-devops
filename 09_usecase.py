92476c8a8da2fb6cc716a25dd144b683628e535c
pavan <vpavan7477@gmail.com>
2024-08-12 16:13:25 +0530

    commit backend files

--- 09_usecase.py
+++ 09_usecase.py
@@ -8,6 +8,6 @@ in_password = input("please enter your_password: ")
 if (in_username == username) and (in_password == password):
     print("Login is successful")
 else: 
-    print("Please check your credentials")
+    print("Login failed!!! Please check your credentials")
 
-    
+
---
+++ 10_loops.py
@@ -0,0 +1,5 @@
+sample_list = ["ansible", "Terraform", "Jenkins", "Docker"]
+
+for ele in sample_list:
+    print(ele, len(ele))
+    
